76d1542ef0432c565f4a06bc4dc850c77df5b038
Jason Mitchell <jvonmitchell@gmail.com>
2013-06-26 16:36:01 -0400

    We modified the code so it would compile

--- profile.c
+++ profile.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/time.h>
 
 char buff[1024];
-char tempbuff[1024]
 
 int main(int argc,char **argv) {
  struct timeval tim;
  struct timeval tim_temp;
  int should_out=0;
- while(!feof(stdin) && fgets(buff,sizeof(buff1),stdin)) {
+ while(!feof(stdin) && fgets(buff,sizeof(buff),stdin)) {
   //Store the time of day.
   gettimeofday(&tim_temp,NULL);
   
   //Print time difference for the previous line.
   if(should_out) {
    suseconds_t usecdif;
-   tv_sec secdif=tim_temp.tv_sec-tim.tv_sec;
+   time_t secdif=tim_temp.tv_sec-tim.tv_sec;
    if(tim_temp.tv_usec<tim.tv_usec) {
     --secdif;
     usecdif=tim.tv_usec-tim_temp.tv_usec;
@@ -24,7 +24,7 @@ int main(int argc,char **argv) {
    else {
     usecdif=tim_temp.tv_usec-tim.tv_usec;
    }
-   fprintf(stdout,"%ds:%du\n",secdif,suseconds_t);
+   fprintf(stdout,"%ds:%du\n",secdif,usecdif);
   }
   tim=tim_temp;  //Store the time of day.
   
