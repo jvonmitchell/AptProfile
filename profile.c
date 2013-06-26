53c0db5ffd65ed9cf8b3fea31e10a82a41b10ab1
Jason Mitchell <jvonmitchell@gmail.com>
2013-06-26 16:44:51 -0400

    We now have the code doing its rudementry behavior correctly.

--- profile.c
+++ profile.c
@@ -3,10 +3,27 @@
 #include <sys/time.h>
 
 char buff[1024];
+struct timeval tim;
+struct timeval tim_temp;
+
+void printtimedif() {
+ suseconds_t usecdif;
+ time_t secdif=tim_temp.tv_sec-tim.tv_sec;
+ if(tim_temp.tv_usec<tim.tv_usec) {
+  --secdif;
+  usecdif=tim.tv_usec-tim_temp.tv_usec;
+  usecdif=1000000-usecdif;
+ }
+ else {
+  usecdif=tim_temp.tv_usec-tim.tv_usec;
+ }
+ fprintf(stdout,"%ds:%du\n",secdif,usecdif);
+}
+
+
 
 int main(int argc,char **argv) {
- struct timeval tim;
- struct timeval tim_temp;
+ 
  int should_out=0;
  while(!feof(stdin) && fgets(buff,sizeof(buff),stdin)) {
   //Store the time of day.
@@ -14,25 +31,16 @@ int main(int argc,char **argv) {
   
   //Print time difference for the previous line.
   if(should_out) {
-   suseconds_t usecdif;
-   time_t secdif=tim_temp.tv_sec-tim.tv_sec;
-   if(tim_temp.tv_usec<tim.tv_usec) {
-    --secdif;
-    usecdif=tim.tv_usec-tim_temp.tv_usec;
-    usecdif=1000000-usecdif;
-   }
-   else {
-    usecdif=tim_temp.tv_usec-tim.tv_usec;
-   }
-   fprintf(stdout,"%ds:%du\n",secdif,usecdif);
-  }
+   printtimedif();
+  } 
+  should_out=1;
   tim=tim_temp;  //Store the time of day.
   
+  
   //Output the buffer.
-  fwrite(buff,1,strlen(buff),stdout);
+  fwrite(buff,1,strlen(buff)-1,stdout);
   fputc('\t',stdout);
  }
- 
+ printtimedif();
 }
-  
-  
+ 
